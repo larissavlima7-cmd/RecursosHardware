9b50beb1b440035f5d0a7ee6815d22487c6d8931
larissavlima7-cmd <larissa.v.lima7@aluno.senai.br>
2026-09-17 10:36:11 -0300

    criação da classe Registro- terminar o factory

---
+++ senai_checkin/lib/Model/registro.dart
@@ -0,0 +1,29 @@
+class Registro {
+  //atributos da classe Registro(as informações de Registro)
+  int? id;
+  String datahora;
+  double latitude;
+  double longitude;
+  String observacao;
+  String caminhoFoto;
+
+//construtor
+  Registro({this.id, required this.datahora, required this.latitude, required this.longitude, required this.observacao, required this.caminhoFoto});
+
+  Map<String, dynamic> toMap(){
+    return{
+      'id': id,
+      'data_hora': datahora,
+      'latitude': latitude,
+      'longitude': longitude,
+      'observacao': observacao,
+      'caminho_foto': caminhoFoto,
+    };
+  }
+
+  factory Registro.fromMap(Map<String, dynamic> Map){
+    return Registro(
+      id: 
+    )
+  }
+}
